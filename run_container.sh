0721d6211e5b88688483afebfe3a2ef38c0ffc7a
user <9879403+mervo@users.noreply.github.com>
2019-08-14 14:40:37 +0800

    updated path of volume to mount.

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,7 @@
+docker build -t see --build-arg NCCL_NAME=nccl-repo-ubuntu1804-2.4.8-ga-cuda10.1_1-1_amd64.deb .
+
 docker run --gpus all \
-  -v /data/projects:/data \
+  -v /data/projects/pixelplus:/data \
   -it see
+  
+  
